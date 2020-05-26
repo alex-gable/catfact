97fd8bf4af475f9a454f6ace6a6c6446f07e3863
alex gable <alex.gable@gmail.com>
2020-05-26 00:40:28 -0700

    add failing condition for duplicate names check

--- tests/testthat/helper-catfact.R
+++ tests/testthat/helper-catfact.R
@@ -1,3 +1,12 @@
 # set up vcr
 library("vcr")
 invisible(vcr::vcr_configure(dir = "../fixtures"))
+
+Sys.setenv(R_CHECK_CASSETTE_NAMES = TRUE)
+
+cat(paste("Does this run check cassettes for duplicates?:",
+          Sys.getenv("R_CHECK_CASSETTE_NAMES")), "\n\n")
+
+if (as.logical(Sys.getenv("R_CHECK_CASSETTE_NAMES"))) {
+  vcr::check_cassette_names()
+}
---
+++ tests/testthat/test-cat_fact_fail.R
@@ -0,0 +1,39 @@
+context("cat_fact")
+
+test_that("cat_fact works", {
+  vcr::use_cassette(name = "cat_fact", {
+    aa <- cat_fact()
+
+    expect_is(aa, "list")
+    expect_named(aa, c('fact', 'length'))
+    expect_is(aa$fact, 'character')
+    expect_type(aa$length, 'integer')
+  })
+})
+
+test_that("cat_fact length works", {
+  vcr::use_cassette(name = "cat_fact_lengths", {
+      expect_lte(cat_fact(30)$length, 30)
+      expect_lte(cat_fact(80)$length, 80)
+      expect_lte(cat_fact(120)$length, 120)
+      expect_lte(cat_fact(300)$length, 300)
+    }, 
+    match_requests_on = c('method', 'uri', 'query'),
+    record = "new_episodes"
+  )
+})
+
+test_that("cat_fact error behavior", {
+  vcr::use_cassette(name = "cat_fact_zero", {
+      aa <- cat_fact(max_length = 0)
+
+      expect_is(aa, "list")
+      expect_equal(length(aa), 0)
+    }, 
+    match_requests_on = c('method', 'uri', 'query'), 
+    preserve_exact_body_bytes = TRUE
+  )
+
+  expect_error(cat_fact(max_length = 'foobar'))
+})
+
